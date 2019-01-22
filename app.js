e1c28e8741330a2395fe8bf391b8523833bf1682
cyber <1725854865@qq.com>
2019-01-22 14:12:16 +0800

    commit

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ App({
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+
+    // wx.getUserInfo({
+    //   success:res=>{
+    //     console.log(res);
+    //   }
+    // });
 
     // 登录
     wx.login({
       success: res => {
+        // console.log("info" , res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -25,7 +32,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              console.log( "ifo" , this.globalData.userInfo );
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
